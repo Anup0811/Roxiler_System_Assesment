d4736780062acccaf1a9fa9d4003f015f1b8a5a4
Mr. Anup Awari <111560237+Anup0811@users.noreply.github.com>
2024-08-19 16:01:44 +0530

    Create ProductTransaction.js

---
+++ backend/models/ProductTransaction.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/Roxiler_System")
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((error) => {
+    console.log("Database Disconnected due to ", error);
+  });
+
+const ProductTransactionSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: mongoose.Types.Decimal128,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  sold: {
+    type: Boolean,
+    required: true,
+  },
+  dateOfSale: {
+    type: Date,
+    required: true,
+  },
+});
+
+const ProductTransaction = mongoose.model(
+  "ProductTransaction",
+  ProductTransactionSchema
+);
+
+module.exports = ProductTransaction;
